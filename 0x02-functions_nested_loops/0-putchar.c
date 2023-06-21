316aa2f5379ef254c926495ffb369e03a6b8fd07
AhmedSamy98 <ahmed28samy@gmail.com>
2023-06-20 19:20:44 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main -  0x02. C - Functions, nested loops
+ * Description : 0. _putchar
+ * Return: 0
+*/
+int main(void)
+{
+	char repet[] = "_putchar";
+	int put;
+
+	for (put = 0; put < 8; put++)
+		_putchar(repet[put]);
+	-putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main -  Tasks
+ * Description : 0x02. C - Functions, nested loops
+ * Return: 0
+*/
+/* more headers goes there */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
