38c3767047f697c0f0bb25a426a425312b4f4626
AhmedSamy98 <ahmed28samy@gmail.com>
2023-06-22 20:14:06 -0700

    #advanced

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,26 +7,28 @@
 */
 int main(void)
 {
-long int n;
-long int m;
-long int i;
-n = 612852475143;
-m = -1;
-while (n % 2 == 0)
-{
-m = 2;
-n /= 2;
-}
-for (i = 3; i <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-m = i;
-n = n / i;
-}
-}
-if (n > 2)
-m = n;
-printf("%ld\n", m);
-return (0);
+	long int n;
+	long int m;
+	long int i;
+
+	n = 612852475143;
+	m = -1;
+
+	while (n % 2 == 0)
+	{
+		m = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			m = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+		m = n;
+	printf("%ld\n", m);
+	return (0);
 }
