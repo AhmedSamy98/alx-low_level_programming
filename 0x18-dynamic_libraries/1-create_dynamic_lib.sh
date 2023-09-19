c3b6e67192bad1bc94e546640fa48a15660a59c4
AhmedSamy98 <ahmed1samy1998@gmail.com>
2023-09-19 05:11:18 +0300

    FIXED

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-c_files=$(ls *.c)
-gcc -shared -o liball.so $c_files
+
+# Compile all .c files in the current directory into object files
+gcc -c *.c
+
+# Create the dynamic library liball.so from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the object files
+rm *.o
